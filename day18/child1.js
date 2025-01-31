c8dc0933e13c94fbb28590f618c3cf48e8b81e21
Praharsha2005 <praharshapillamari844@gmail.com>
2025-01-31 20:03:08 +0530

    Add files via upload

---
+++ day18/child1.js
@@ -0,0 +1,43 @@
+// let x = 10;
+// export default x;
+
+// let userDetail={
+//     name:"Sathvik",
+//     profession:"CR"
+// }
+// export default userDetail;
+
+// !named export
+// let x = 10;
+
+// const user ="Praharsha";
+// const users = ["Prabhakar", "Shailaja", "Praharsha", "Prajit"];
+
+// const obj={
+//     name:"Praharsha",
+//     company:"Cognizent"
+// }
+
+// !named export
+// export{
+//     x,
+//     user,
+//     users,
+//     obj
+// }
+
+
+
+// !named export 2nd way
+
+export let x = 10;
+export const user="Praharsha";
+
+export const users=["Harsha", "Kajal"];
+export const obj={
+    name:"Sam",
+    company:"ExcelR"
+}
+
+let college = "Malla Reddy University";
+export default college;
